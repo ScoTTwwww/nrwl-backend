4a9ac18e1e9ca3a4fae95f0c59874e9fbd328109
scott <scott21739@gmail.com>
2020-09-29 00:29:17 +0800

    fixed

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,8 +2,8 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
-import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/user.entity';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
